e6ef0879cee28453cb44bfc4c42ee502c5d0ae4a
Bram Devlaminck <bram.devlaminck@gmail.com>
2023-02-19 19:58:49 +0100

    Updates

---
+++ app_/immutable/chunks/0-c6801ec5.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-da46b06b.js";import{default as t}from"../components/pages/_layout.svelte-0d0fd73e.js";export{t as component,r as universal};
---
+++ app_/immutable/chunks/1-8f1b6d6c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-bf4cc1e5.js";export{t as component};
